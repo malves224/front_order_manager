1f7ca98427c4d779f6a38398c5b97b31c57827b5
Matheus Alves <malves224@gmail.com>
2024-02-22 19:13:46 -0300

    organize components admin and gestor

---
+++ src/views/AdminView.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    diministrador
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ src/views/GestorView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    respeita gestor karai
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style></style>
