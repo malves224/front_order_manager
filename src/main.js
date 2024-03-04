ed16739b705d5d31cf5b43433000e4458cecb21b
Matheus Alves <malves224@gmail.com>
2024-03-04 10:34:57 -0300

    install pinia

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import formatCurrency from './mixins/currencyFormatter'
+import { createPinia, PiniaVuePlugin } from 'pinia'
 
 Vue.config.productionTip = false
 
@@ -10,9 +11,11 @@ import 'bootstrap-vue/dist/bootstrap-vue.css'
 import router from './router'
 
 Vue.prototype.$formatCurrency = formatCurrency
+Vue.use(PiniaVuePlugin)
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
 new Vue({
   router,
+  pinia: createPinia(),
   render: h => h(App)
 }).$mount('#app')
