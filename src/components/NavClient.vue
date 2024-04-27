256c7db8a3ef71ec1eebd986ecedb9805441e655
Matheus Alves <malves224@gmail.com>
2024-04-27 13:19:52 -0300

    Feat(NavCart)private cart navigation

--- src/components/NavClient.vue
+++ src/components/NavClient.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="position-nav">
-    <div class="cart-info" v-if="cartStore.total">
-      <div class="cart-value">
-        <p>Total sem a entrega</p>
-        <div class="value-info">
-          <h6>{{ $formatCurrency(cartStore.total) }}</h6>
-          <p>/ {{ cartStore.count }} item{{ cartStore.total > 1 ? 's' : '' }}</p>
-        </div>
-      </div>
-      <b-button class="ms-3" to="cart" variant="primary">Ver carrinho</b-button>
-    </div>
+    <NavCart />
     <div class="nav-client">
       <div class="nav-item">
         <router-link to="/order">Inicio</router-link>
@@ -21,20 +12,12 @@
   </div>
 </template>
 <script>
-import { useCartStore } from '@/store/cart';
-import { mapStores } from 'pinia';
+import NavCart from '@/components/NavCart.vue'
+
 export default {
-  data() {
-    return {
-      hasCart: true,
-    }
+  components: {
+    NavCart
   },
-  mounted() {
-    this.cartStore.initializeFromLocalStorage();
-  },
-  computed: {
-    ...mapStores(useCartStore)
-  }
 }
 </script>
 <style>
@@ -48,31 +31,6 @@ export default {
   flex-flow: column-reverse;
 }
 
-.cart-info {
-  background-color: white;
-  display: flex;
-  justify-content: space-between;
-  padding: 8px 10px;
-  height: 50px;
-  order: 1
-}
-
-.cart-info>a {
-  flex-grow: 1;
-}
-
-.cart-value p {
-  font-size: 12px;
-  text-align: start;
-}
-
-.value-info {
-  display: flex;
-}
-
-.value-info>h6 {
-  margin: 0;
-}
 
 .nav-client {
   background-color: rgb(255, 255, 255);
