316fa3437eea4ecdad460a64d2d01d5ba4b5b4a3
Matheus Alves <malves224@gmail.com>
2024-05-19 16:08:39 -0300

    feat(ConfirmOrder)Add address form action

--- src/views/ConfirmOrder.vue
+++ src/views/ConfirmOrder.vue
@@ -1,13 +1,38 @@
 <template lang="">
-    <div>
-        faz o éliiiiiiiiiiiiiiiiii
+    <div class="mt-3 confirm-container">
+        <h4>Endereço de entrega</h4>
+        <div class="address-list">
+          <div v-for="address in addresses" :key="address.id">
+            <p>{{address}}</p>
+          </div>
+        </div>
+        <b-button @click="showFormAddress = !showFormAddress" class="mt-2 align-self-center button-none">Adicionar endereço</b-button>
+        <van-action-sheet style="height: 80vh" v-model="showFormAddress" title="Title">
+          <div class="content">
+            <AddressForm />
+          </div>
+        </van-action-sheet>
     </div>
 </template>
 <script>
-export default {
+import AddressForm from "@/components/AddressForm.vue";
 
+export default {
+  components: {
+    AddressForm
+  },
+  data() {
+    return {
+      addresses: [],
+      showFormAddress: false
+    }
+  }
 }
 </script>
-<style lang="">
-    
+<style>
+.confirm-container {
+  display: flex;
+  flex-flow: column;
+
+}
 </style>
