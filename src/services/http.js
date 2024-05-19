bcc0ddf794983dfd36c07d326c9339f9c233dd40
Matheus Alves <malves224@gmail.com>
2024-05-19 16:06:03 -0300

    feat(http)acept url

--- src/services/http.js
+++ src/services/http.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 class HttpService {
-  constructor() {
+  constructor(url = undefined) {
     this.client = axios.create({
-      baseURL: process.env.VUE_APP_API_URL || 'http://192.168.15.25:3000/'
+      baseURL: url ? url : process.env.VUE_APP_API_URL || 'http://192.168.15.25:3000/'
     })
   }
 
